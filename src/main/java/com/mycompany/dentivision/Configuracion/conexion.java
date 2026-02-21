2943a557b84034700543338c7482838613fd155f
christian reyes <creyese23@gmail.com>
2026-02-21 10:31:31 -0500

    Actualizacion de  archivos del proyecto

--- src/main/java/com/mycompany/dentivision/Configuracion/conexion.java
+++ src/main/java/com/mycompany/dentivision/Configuracion/conexion.java
@@ -5,27 +5,28 @@
 package com.mycompany.dentivision.Configuracion;
 
 import java.sql.*;
+
 /**
  *
  * @author Creyese23
  */
 public class conexion {
-   
-    
-    public static Connection getConexion(){
-        
-    String URL = "jdbc:mysql://localhost:3306/registro";
-    String User = "root";
-    String Psw = "";
-        try{
+
+    public static Connection getConexion() {
+        String URL = "jdbc:mysql://localhost:3306/registro";
+        String User = "root";
+        String Psw = "";
+
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, User, Psw);
-        }catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             System.out.println("No fue posible cargar el driver.");
 
         } catch (SQLException e) {
             System.out.println("Hubo un error al acceder a la base de datos: " + e.getMessage());
         }
+
         return null;
     }
 }
--- src/main/java/com/mycompany/dentivision/Dentivision.java
+++ src/main/java/com/mycompany/dentivision/Dentivision.java
@@ -15,47 +15,71 @@ public class Dentivision {
 
     public static void main(String[] args) {
 
-        Scanner tec = new Scanner(System.in);
-        
-        registrar_usuarios crear = new registrar_usuarios();
-        
-        int op;
-        boolean salir = false;
-        String td, doc, nom, ap, correo, conf_correo, contrasena,conf_cont = "";
-        
-        do {            
-            System.out.println("|------------- MENU PRINCIPAL-------------|");
-            System.out.println("|------------- 1. Crear Usuarios ------------|");
-            System.out.println("|------------- 2. Listar Usuarios ------------|");
-            System.out.println("|------------- 3. Actualizar Usuarios -------|");
-            System.out.println("|------------- 4. Eliminar Usuarios ---------|");
-            
-            op = tec.nextInt();
-            
-            switch (op) {
-                case 1:
-                    System.out.println("Digite los datos del usuario");
-                    System.out.println("Nombres: ");
-                    td = tec.next();
-                    crear.setTipo_documento(td);
-                    
-                    
-                    
-                    break;
-                  case 2:
-                      
-                      break;
-                  case 3:
-                      
-                      break;
-                  case 4:
-                      
-                      break;
-                  
-                default:
-                    throw new AssertionError();
-            }
-        } while (salir);
+        try {
+            Scanner tec = new Scanner(System.in);
+
+            registrar_usuarios ingresar = new registrar_usuarios();
+            Crud listar = new Crud();
+
+            int op;
+            boolean salir = false;
+            String td, doc, nom, ap, correo, conf_correo, contrasena, conf_cont = "";
+
+            do {
+
+                System.out.println("|------------- MENU PRINCIPAL-------------|");
+                System.out.println("|------------- 1. Crear Usuarios ------------|");
+                System.out.println("|------------- 2. Listar Usuarios ------------|");
+                System.out.println("|------------- 3. Actualizar Usuarios -------|");
+                System.out.println("|------------- 4. Eliminar Usuarios ---------|");
+
+                System.out.println("Escoja una opcion: ");
+                op = tec.nextInt();
+
+                switch (op) {
+                    case 1:
+                        System.out.println("Digite los datos del usuario");
+                        System.out.println("Tipo de documento: ");
+                        td = tec.next();
+                        ingresar.setTipo_documento(td);
+                        System.out.println("Numero documento: ");
+                        doc = tec.next();
+                        ingresar.setDocumento(Integer.parseInt(doc));
+                        System.out.println("Nombres: ");
+                        nom = tec.next();
+                        ingresar.setNombres(nom);
+                        System.out.println("Apellidos: ");
+                        ap = tec.next();
+                        ingresar.setApellidos(ap);
+                        System.out.println("Correo: ");
+                        correo = tec.next();
+                        ingresar.setCorreo(correo);
+                        System.out.println("Confirmar Correo: ");
+                        conf_correo = tec.next();
+                        ingresar.setConfirmar_correo(conf_correo);
+                        System.out.println("Contrasena: ");
+                        contrasena = tec.next();
+                        ingresar.setContrasena(contrasena);
+                        System.out.println("Confirmar Contrasena");
+                        conf_cont = tec.next();
+                        ingresar.setConfirmar_contrasena(conf_cont);
+                        break;
+                    case 2:
+                        listar.leer();
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    default:
+                        System.out.println("Opcion invalida.");
+                }
+            } while (true);
+        } catch (Exception e) {
+            //e.printStackTrace();
+        }
 
     }
 }
